0b9062b7d82c460d725a7f1d1456cb3614c0becd
ebisLab <ebaiden@techie.com>
2021-03-14 13:05:10 -0400

    clean up

--- knexfile.js
+++ knexfile.js
@@ -5,26 +5,6 @@ const pgConnection = process.env.DATABASE_URL;
 // if using a local postgres server, please create the database manually, Knex will not create it autmatically
 
 module.exports = {
-  //   development: {
-  //   client: "pg",
-  //   useNullAsDefault: true,
-  //   connection: process.env.DATABASE_URL,
-  //   pool: {
-  //     min:2,
-  //     max:10
-  //     // afterCreate: (conn, done) => {
-  //     //   conn.run("PRAGMA foreign_keys = ON", done);
-  //     // },
-  //   },
-  //   migrations: {
-  //     directory: "./database/migrations",
-  //     tableName: "knex_migrations"
-  //   },
-  //   seeds: {
-  //     directory: "./database/seeds",
-  //   },
-  // },
-
   development: {
     client: "sqlite3",
     useNullAsDefault: true,
@@ -46,12 +26,10 @@ module.exports = {
 
   production: {
     client: "pg",
-    // connection: pgConnection,
     connection: { 
-      connectionString: process.env.DATABASE_URL,
+      connectionString: pgConnection,
       ssl: { rejectUnauthorized: false }, 
       },
-    // ssl: { rejectUnauthorized: false },
     pool: {
       min: 2,
       max: 10,
