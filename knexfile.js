fd4b98062006f6b50953e0148d34fba6d6c86ec3
ebisLab <ebaiden@techie.com>
2021-03-13 21:53:29 -0500

    ssl query

--- knexfile.js
+++ knexfile.js
@@ -46,8 +46,7 @@ module.exports = {
 
   production: {
     client: "pg",
-    connection: pgConnection,
-    ssl:true,
+    connection: pgConnection+"?ssl=true",
     pool: {
       min: 2,
       max: 10,
