a617728a235cf78b13cb7bdbc9d03528fcced0b8
ebisLab <ebaiden@techie.com>
2021-03-13 21:20:53 -0500

    knexfile added ssl:true

--- knexfile.js
+++ knexfile.js
@@ -5,48 +5,48 @@ const pgConnection = process.env.DATABASE_URL;
 // if using a local postgres server, please create the database manually, Knex will not create it autmatically
 
 module.exports = {
-    development: {
-    client: "pg",
-    useNullAsDefault: true,
-    connection: process.env.DATABASE_URL,
-    pool: {
-      min:2,
-      max:10
-      // afterCreate: (conn, done) => {
-      //   conn.run("PRAGMA foreign_keys = ON", done);
-      // },
-    },
-    migrations: {
-      directory: "./database/migrations",
-      tableName: "knex_migrations"
-    },
-    seeds: {
-      directory: "./database/seeds",
-    },
-  },
-
-  // development: {
-  //   client: "sqlite3",
+  //   development: {
+  //   client: "pg",
   //   useNullAsDefault: true,
-  //   connection: {
-  //     filename: "./database/auth.db3",
-  //   },
+  //   connection: process.env.DATABASE_URL,
   //   pool: {
-  //     afterCreate: (conn, done) => {
-  //       conn.run("PRAGMA foreign_keys = ON", done);
-  //     },
+  //     min:2,
+  //     max:10
+  //     // afterCreate: (conn, done) => {
+  //     //   conn.run("PRAGMA foreign_keys = ON", done);
+  //     // },
   //   },
   //   migrations: {
   //     directory: "./database/migrations",
+  //     tableName: "knex_migrations"
   //   },
   //   seeds: {
   //     directory: "./database/seeds",
   //   },
   // },
 
+  development: {
+    client: "sqlite3",
+    useNullAsDefault: true,
+    connection: {
+      filename: "./database/auth.db3",
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      },
+    },
+    migrations: {
+      directory: "./database/migrations",
+    },
+    seeds: {
+      directory: "./database/seeds",
+    },
+  },
+
   production: {
     client: "pg",
-    connection: pgConnection,
+    connection: {...pgConnection, ssl:true},
     pool: {
       min: 2,
       max: 10,
